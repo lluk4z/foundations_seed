843790578bcea11b80ee03c62926c700ed2a7099
lluk4z <lemmg@cin.ufpe.br>
2022-09-13 11:41:13 -0300

    base do projeto

---
+++ Shared/seed_foundationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  seed_foundationsApp.swift
+//  Shared
+//
+//  Created by Lucas Emanuel on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct seed_foundationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
