2fdf1b96ee8753ae0c311c9892787bd840ac1790
lluk4z <lemmg@cin.ufpe.br>
2022-09-15 10:26:34 -0300

    Lista da home

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let cards = card_info.listarTodos()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+        Text("Seed").padding()
+        List(cards){ card in
+            HStack {
+                Image(card.iconImg)
+                    .aspectRatio(contentMode: .fit)
+                VStack{
+                    Text(card.title)
+                        .fontWeight(.regular)
+                        .multilineTextAlignment(.leading)
+                        .padding([.leading, .bottom, .trailing], 1.0)
+                        
+                    Text(card.textPreview)
+                        .font(.caption)
+                        .fontWeight(.thin)
+                        .multilineTextAlignment(.leading)
+                        .padding(.leading)
+                    
+                    HStack{
+                    Image(card.imgPrev[0]).padding()
+                    Image(card.imgPrev[1])
+                    }
+                        
+                }
+                
+            }
+            
+        }
+        }
     }
 }
 
--- Shared/seed_foundationsApp.swift
+++ Shared/seed_foundationsApp.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+struct card_info: Identifiable {
+    var id: UUID = UUID()
+    let iconImg: String
+    let title: String
+    let textPreview: String
+    let imgPrev: [String] = ["img01", "img02"]
+    
+    static func listarTodos() -> [card_info] {
+        return [
+            card_info(iconImg: "logoApple", title: "Academy - UFPE", textPreview: "A Apple Developer Academy é um projeto de capacitação criado em parceria entre o Centro de Infor....")
+        ]
+    }
+    
+}
+
 @main
 struct seed_foundationsApp: App {
     var body: some Scene {
